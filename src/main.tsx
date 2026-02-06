c4df4d92200fde530bc3021890974c0174ba0c70
mgbmediagroup <mgbmediagroup@gmail.com>
2026-02-06 20:51:36 +0100

    Add debug logging to verify title on load - MGB Media Group

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,8 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+  // Debug log to verify title on load
+  console.log("TITLE ON LOAD:", document.title);
+
   createRoot(document.getElementById("root")!).render(<App />);
   
