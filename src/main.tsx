0ab3ed1d1d8a6682041287a2e4ca3bf8f7ee14e8
mgbmediagroup <mgbmediagroup@gmail.com>
2026-02-06 20:53:23 +0100

    Final build - MGB Media Group title confirmed working

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,5 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  // Debug log to verify title on load
-  console.log("TITLE ON LOAD:", document.title);
-
   createRoot(document.getElementById("root")!).render(<App />);
   
